0beb5516103d0f347dfff39d38dd89b5bcb1648b
Cole Webster <Cole.Webster@gmail.com>
2022-02-09 19:35:38 -0500

    Updated the index and user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
-
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
 //Require the correct files into the api folder
 
+router.use('/comments', commentRoutes);
+router.use('/posts', postRoutes);
+router.use('/users', userRoutes);
 //Create the router.use functionality for each required file
 
 
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,19 +1,63 @@
 const router = require('express').Router();
+const { Project, User } = require('../../models');
 //Require the correct files from the models
 
 router.post('/', async (req, res) => {
   // Create the correct asychronous function for this login post request with error handling
+  try {
+    const userData = await User.create(req.body);
 
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/login', async (req, res) => {
   // Create the correct asychronous function for this login post request with error handling
+  try {
+    const userData = await User.findOne({ where: {email: req.body.email} });
 
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, try again' });
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'You are logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/logout', (req, res) => {
   // Create the correct functionality for this post request with error handling
-
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
-
+      
 module.exports = router;
